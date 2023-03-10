e97fa8e4c74a4c6dac3e4705494520888effa541
rafaaulbrich <rafaelacamilly2018@gmail.com>
2023-03-10 11:41:53 -0300

    commit-1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,68 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from 'vue'
+    const nome = 'Rafaella'
+    const idade = 16
+
+    function inverter(texto) {
+        return texto.split('').reverse().join('')
+    }
+
+    function saudacao() {
+        return `Olá, ${nome}!`
+    }
+
+    let valor1 = ref("");
+    function mostrarDobro() {
+      return `O dobro do ${valor1.value} é ${valor1.value * 2}`
+    }
+    
+    let valor2 = ref('');
+    let valor3 = ref("");
+    function soma() {
+      return `A soma dos números resulta em ${(valor2.value * 1) + (valor3.value * 1)}`;
+    } 
+    function somaPar() {
+      if (soma.value % 2 === 0) {
+      return `A soma é par`
+    }}
+    
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="info">
+    <h1>Exemplo 1 - Mostrando informações na tela</h1>
+    <p>Nome: {{ nome }} </p>
+    <p>Idade: {{ idade }}  anos</p>
+    <hr />
+    <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+    <p>O nome tem {{ nome.length }}  caracteres</p>
+    <p>O nome invertido é: {{ (inverter(nome)).toUpperCase() }} </p>
+    <hr />
+    <p> Exemplo de saudação usando função</p>
+    <p> {{ saudacao() }}  </p>
+    <hr />
+    <input v-model="valor1">
+    <p> {{ mostrarDobro() }} </p>
+    <hr />
+    <input v-model="valor2">
+    <input v-model="valor3">
+    <p> {{ soma() }} </p>
+    <hr />
+    <p>{{ somaPar }}</p>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.info {
+  background-color: rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 1.5em;
 }
 </style>
